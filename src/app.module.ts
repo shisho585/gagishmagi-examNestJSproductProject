21e6b4fc70f6b84cdba54102ad01ec871175b699
gagi <gagi.shmagi@gmail.com>
2020-11-12 00:04:34 +0200

    Please setup db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: '????',
+      host: '?????',
+      port: '?????',
+      username: '??????',
+      password: '??????',
+      database: 'test_db',
+      entities: '??',
+      synchronize: '?????',
+    }),
+    ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
